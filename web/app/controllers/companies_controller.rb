4d04b22c41c6fd8aca44caec85cbf365b838cdd4
Alain Mimeault <lhorde@msn.com>
2026-05-26 17:10:27 +0900

    build redimentary index page that shows a card with each company's info> some is hard-coded

--- web/app/controllers/companies_controller.rb
+++ web/app/controllers/companies_controller.rb
@@ -1,7 +1,7 @@
 class CompaniesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index
-    @companies = Company.all
-    authorize @company
+    @companies = policy_scope(Company)
     # query function might be needed at some point
   end
 
