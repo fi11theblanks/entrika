0a009034fe8c4c59215497cde0e06d925c599964
Alain Mimeault <lhorde@msn.com>
2026-05-26 12:31:58 +0900

    Write routes.rb file with all nested associations, including for the api

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :companies, only: [:index, :show] do
+    resources :messages, only: [:create]
+  end
+  resources :registrations, only: [:update]
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :companies, only: [:show, :create] do
+        resources :registrations, only: [:create]
+      end
+      devise_for :users,
+        path: "",
+        path_name_names: {
+          sign_in: "login",
+          sign_out: "logout"
+        },
+        controllers: {
+          sessions: "api/v1/users/sessions"
+      }
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "dashboard", to: "registration#index", as: :dashboard
 end
