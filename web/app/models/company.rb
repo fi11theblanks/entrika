5b820d9215de2049e71406854eb102d6247d3c7d
AydaSelen Pilanci <aydaselen.pilanci@gmail.com>
2026-05-26 10:50:06 +0900

    added TOS scraper service and updated seeds to use it

--- web/app/models/company.rb
+++ web/app/models/company.rb
@@ -3,4 +3,5 @@ class Company < ApplicationRecord
   has_many :users, through: :registrations
   validates :url, presence: true
   validates :name, presence: true
+  has_one_attached :tos_document
 end
