58210724d2fcaefb794d4175d011fa22ff725b51
AydaSelen Pilanci <aydaselen.pilanci@gmail.com>
2026-05-27 16:39:42 +0900

    fixed companies more

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -6,13 +6,4 @@ User.find_or_create_by!(email: "test@example.com") do |user|
   user.username = "testuser"
 end
 
-TosScraper.scrape_all
-
-COMPANY_ANALYSIS_DATA.each do |name, data|
-  company = Company.find_by(name: name)
-  next unless company
-  company.update!(data.merge(
-    summary: "Terms of Service Summary:\n#{data[:tos_summary]}\n\nPrivacy Policy Summary:\n#{data[:privacy_summary]}",
-    analysis: "Terms of Service Analysis:\n#{data[:tos_analysis]}\n\nPrivacy Policy Analysis:\n#{data[:privacy_analysis]}"
-  ))
-end
+#TosScraper.scrape_all
