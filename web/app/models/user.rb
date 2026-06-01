98cdfe2c56acee6f88e9145e084a35a1b9d7fe87
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-06-01 15:50:49 +0900

    added overall risk assessment and card to dashboard, edited the dashboard scss in accordance as well

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -8,4 +8,21 @@ class User < ApplicationRecord
   has_many :companies, through: :registrations
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def registered_risk_score
+    return 0 unless registered_companies.any?
+
+    score = (Company.where(id: registered_companies).pluck(:risk_score).sum / registered_companies.count).round(2)
+
+    case score
+    when 0.0..1.5 then "Low Risk"
+    when 1.5..2.5 then "Medium Risk"
+    when 2.5..3.0 then "High Risk"
+    else "Unknown"
+    end
+  end
+
+  def registered_companies
+    registrations.where(status: 'registered').pluck(:company_id)
+  end
 end
