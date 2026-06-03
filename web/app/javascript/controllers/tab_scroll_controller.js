c881ebf6fbde1d0abb1aa38a6a186dd387f0a082
millarchie <archie.millar@hotmail.com>
2026-06-03 17:07:47 +0900

    slight tweak, commented out currently unnecessary js in scroll controller. might need back in after restyling applied

--- web/app/javascript/controllers/tab_scroll_controller.js
+++ web/app/javascript/controllers/tab_scroll_controller.js
@@ -19,16 +19,16 @@ export default class extends Controller {
         // Scroll the card into view at the top
         firstCard.scrollIntoView({ behavior: 'smooth', block: 'start' })
 
-        // Add top padding by scrolling up a bit
-        setTimeout(() => {
-          window.scrollBy({ top: -100, behavior: 'smooth' })
-        }, 300)
+        // // Add top padding by scrolling up a bit
+        // setTimeout(() => {
+        //   window.scrollBy({ top: -100, behavior: 'smooth' })
+        // }, 300)
 
-        // Highlight the card briefly
-        firstCard.style.backgroundColor = '#f0f0f0'
-        setTimeout(() => {
-          firstCard.style.backgroundColor = ''
-        }, 2000)
+        // // Highlight the card briefly
+        // firstCard.style.backgroundColor = '#f0f0f0'
+        // setTimeout(() => {
+        //   firstCard.style.backgroundColor = ''
+        // }, 2000)
       }
     }
   }
