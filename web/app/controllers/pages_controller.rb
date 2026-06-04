976a60b4aeb7275f5403894ccb4742de0be3bf34
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-06-04 14:24:52 +0900

    turned donut chart into column chart, changed dashboard risk assessment overview title to dashboard overiew, added personal risk score text to personal risk score card, added high risk signed up sites on dashboard

--- web/app/controllers/pages_controller.rb
+++ web/app/controllers/pages_controller.rb
@@ -8,5 +8,6 @@ class PagesController < ApplicationController
     @registrations = policy_scope(Registration)
     @sorted_registrations = @registrations.order(updated_at: :desc).first(3)
     @risk_data = current_user.risk_score_chart_data
+    @high_risk_registrations = current_user.high_risk_registrations
   end
 end
