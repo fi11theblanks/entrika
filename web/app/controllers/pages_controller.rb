09639361cb23742aaf1c06bc80233f3b25702528
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-06-04 15:24:13 +0900

    added horizontal scrolling to high risk signed up sites display, made registered sites display into grid

--- web/app/controllers/pages_controller.rb
+++ web/app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @registrations = policy_scope(Registration)
-    @sorted_registrations = @registrations.order(updated_at: :desc).first(3)
+    @sorted_registrations = @registrations.order(updated_at: :desc).first(4)
     @risk_data = current_user.risk_score_chart_data
     @high_risk_registrations = current_user.high_risk_registrations
   end
